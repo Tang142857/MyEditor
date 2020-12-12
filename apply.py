d454467571205b5a13f8ab8e15548533c8b56fa0
Tang142857 <dfsa00001@qq.com>
2020-12-12 17:20:20 +0800

    add something

--- apply.py
+++ apply.py
@@ -3,3 +3,9 @@ TextbookChecler
 @author(Please add your name after this): Tang142857
 (c) DFSA Software Develop Center
 """
+import tkinter
+
+
+class MainWindow(object):
+    def __init__(self, tks: tkinter.Tk):
+        self.tks = tks
