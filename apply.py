4ad230bdc64dc2f31cea139d691542812a8f91fc
Tang142857 <dfsa00001@qq.com>
2021-01-24 20:00:23 +0800

    add file object

--- apply.py
+++ apply.py
@@ -10,73 +10,113 @@ import os
 import sys
 import tkinter
 import tkinter.filedialog
+import tkinter.messagebox
 
+import exceptions
 from coreEditor import editor
 from coreElement import ui
 from coreElement.mainEvent import EditEvent
-import exceptions
 
 sys.path.append(os.getcwd())  # reset the 'include path' the load the extend
-
-RUN_STATUS = {'isOpened': False, 'isSaved': False, 'openedFilePath': ''}
 
 
 class TextFile(object):
     """文本文件类，用于打开，保存文件，以及储存文件状态（替代RUN_STATUS）和更多文件服务"""
-    def __init__(self,isNew=True,path=None):
+    def __init__(self, isNew=True, path='Untitle.txt'):
         """
         初始化文件
         isNew: 是否创建新文件，默认创建新文件
         path: 若读取文件，需要传入path，创建的新文件没有path（None），保存时报错
         """
+        self.path = path
+        self.isOpened = True
+        self.isSave = True
+
+        self.bitFile = None
+        self.strFile = None
+        # create variables end
+        if isNew is not True:
+            self.__loadFile()
+            UI_WIDGETS.contentViewText.insert('1.0', self.strFile)
+        else:
+            log('Create empty file.')
+
+        UI_WIDGETS.relieveEmptyText()
+
+    def __loadFile(self):
+        with open(self.path, 'rb') as f:
+            self.bitFile = f.read()  # open with bin for encode after load
+
+        try:
+            self.strFile = self.bitFile.decode(encoding='utf-8')
+        except BaseException as msg:  # I really don't know what is the exception's name
+            tkinter.messagebox.showerror('Open error:decode!!!', msg)
+            raise exceptions.OpenFileException('Can not decode the file ,please check decode.')
+
+    def save(self, encoding='utf-8'):
+        """Save the file with path"""
+        content = UI_WIDGETS.contentViewText.get('1.0', 'end')[:-1]  # need not the last \n
+        print([content])
+
+        if self.path == 'Untitle.txt':
+            self.path = tkinter.filedialog.asksaveasfilename(title='Save new file')
+            if self.path == '': return  # user choose cancal
+
+        with open(self.path, 'wb') as f:
+            f.write(content.encode(encoding))
+            self.strFile = content  # update memory
+        self.isSave = True
+
+    def edit(self, text, event):
+        self.isSave = False
+
+    def close(self):
+        """Make sure file is saved"""
+        if self.isSave is False:
+            ans = tkinter.messagebox.askyesno('Save', 'File has not saved,save it right now?\n文件未保存，保存？')
+            if ans: self.save(UI_WIDGETS.contentViewText)  # save file
+        del self.bitFile, self.strFile, self.path, self.isSave, self.isOpened
+        UI_WIDGETS.contentViewText.delete('1.0', 'end')
+        UI_WIDGETS.fillEmptyText()
 
 
 def openFile(path=None):
     """Clean up the text and insert new file"""
     try:
-        closeFile()  # close the last file
-    except exceptions.CloseFileException:
-        pass
-    finally:
-        UI_WIDGETS.relieveEmptyText()  # relieve the content viewer
+        closeFile()
+    except exceptions.CloseFileException as msg:
+        log(msg.__str__())  # 报错多半是首次打开，没文件可以关
 
     if (path is None) or (not os.path.isfile(path)):
-        path = tkinter.filedialog.askopenfilename(title='Open')
-        if path == '':
-            return  # 用户点击取消
+        path = tkinter.filedialog.askopenfilename(title='Open new file')
+        if path == '': return  # user choose 'cancl'
 
-    with open(path, 'r', encoding='utf-8') as f:
-        content = f.read()
-    UI_WIDGETS.contentViewText.insert('1.0', content)
-    # read file and write them into text
+    global FILE  # 向file指针上面挂，不然函数返回就没了
+    FILE = TextFile(False, path)
 
-    RUN_STATUS['isOpened'] = True
-    RUN_STATUS['openedFilePath'] = path
     # save file options
-    editEvent.emit()
+    editEvent.emit('Call by apply.openFile')
     log(f'Opened file {path} successfully')
 
 
 def closeFile():
     """Clean up the window"""
-    if RUN_STATUS['isOpened'] is False:
+    try:
+        FILE.close()
+    except AttributeError:  # first run
         raise exceptions.CloseFileException('Have not opened a file.')
-    else:
-        UI_WIDGETS.contentViewText.delete('1.0', 'end')
-        UI_WIDGETS.fillEmptyText()
 
-        RUN_STATUS['isOpened'] = False
-        RUN_STATUS['openedFilePath'] = ''
-        # Change status to closed
-        log('Closed file successfully')
+    log('Closed file successfully')
+
+
+def save():
+    """Save file with text in text"""
+    FILE.save()
+    log('save file')
 
 
 def openWorkDir():
     log('open work dir')
-
-
-def save():
-    log('save file')
 
 
 def copyContent():
@@ -95,7 +135,9 @@ def log(message):
 if __name__ == '__main__':
     MAIN_WINDOW = tkinter.Tk()
     UI_WIDGETS = ui.MainWidgets(MAIN_WINDOW)
+    UI_WIDGETS.fillEmptyText()
     editor.setTags(UI_WIDGETS.contentViewText)  # config tags for color
+    FILE = TextFile()  # point to text file in order not to let it deleted
 
     # Some event emitted by MAIN_WINDOW should create for extend(include editor)
     editEvent = EditEvent(UI_WIDGETS.contentViewText)
@@ -112,6 +154,5 @@ if __name__ == '__main__':
     # connect functions end
 
     MAIN_WINDOW.bind('<KeyRelease>', editEvent.emit)
-    UI_WIDGETS.fillEmptyText('Welcome')
 
     MAIN_WINDOW.mainloop()  # 主循环
