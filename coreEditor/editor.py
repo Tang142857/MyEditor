361d1233efaeb0f3ac5eb4b0e86e7527a9131158
Tang142857 <dfsa00001@qq.com>
2021-01-25 23:52:54 +0800

    fix 多重括号

--- coreEditor/editor.py
+++ coreEditor/editor.py
@@ -14,7 +14,76 @@ from coreElement.mainEvent import EditorLogEvent
 logEvent = EditorLogEvent()
 SPECIAL_CHARS = [r'\!', r'\@', r'\#', r'\$', r'\%', r'\^', r'\&', r'\*', r'\d']  # 这里为了使用re就只能写成两个字符，一会要特殊计算偏移量
 KEY_WORDS = ['main', 'if', '小说', '·', '：', ':']
-SPECIAL_RANGE = [("'", "'"), ('"', '"'), ("“", "”"), ("(", ")"), ('‘', '’')]
+SPECIAL_RANGE = [("'", "'", True), ('"', '"', True), ("“", "”", True), ("(", ")", False), ('‘', '’', True)]
+
+
+# inline start ,you needn't pay attention to here :)
+class stack(object):
+    def __init__(self):
+        self.mainList = []  # a list to save Python object
+
+    def put(self, obj):
+        self.mainList.append(obj)
+
+    def get(self):
+        lastDate = self.mainList[-1]
+        del self.mainList[-1]
+        return lastDate
+
+    def isEmpty(self):
+        if self.mainList == []:
+            return True
+        else:
+            return False
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.mainList != []:
+            return self.get()
+        else:
+            raise StopIteration
+
+
+def startSymbol(start):
+    def endSymbol(end):
+        return [start, end]
+
+    return endSymbol
+
+
+def __findArea(string: str, start="(", end=")", near=True):
+    """
+    在字符串中匹配start和end包围起来的内容（括号，人物对话之类的）\n
+    near:是否需要最近匹配，例如字符串引号就是最近，括号则是最远
+    ouput:[start index,end index]
+
+    这东西的逻辑复杂，首先，正常情况下应该先出现start，然后end，start
+    时创建修饰器保存index，到了end时，就取出最近的
+    start并调用生成output。不正常的很好判断，直接从头到尾就行了
+    """
+    indexStack = stack()  # cell [start index,end index]
+    indexList = []
+
+    for index, char in enumerate(string):
+        if char == start and (indexStack.isEmpty() or (not near)):
+            indexStack.put(startSymbol(index))
+        elif char == end:
+            if indexStack.isEmpty():  # 空的说明没有前符号
+                indexList.append([0, index])
+            else:  # 正常，取出最近start
+                indexList.append((indexStack.get())(index))
+        else:
+            pass
+
+    for remain in indexStack:
+        indexList.append(remain(index))
+
+    return indexList
+
+
+# inline end
 
 
 def setTags(text):
@@ -23,37 +92,6 @@ def setTags(text):
     text.tag_config('bracket', foreground='blue', background='red')
     text.tag_config('key', foreground='orange', underline=True)
     text.tag_config('warning', foreground='red', background='yellow')
-
-
-def __findArea(string: str, start='“', end='”'):  # FIXME 多重括号！！！
-    """在字符串中匹配start和end包围起来的内容（括号，人物对话之类的）"""
-    indexList = []  # cell [start index,end index]
-    inRange = False
-    cell = [0, 0]
-
-    for index, char in enumerate(string):
-        if char == start and (not inRange):  # enter range
-            inRange = True
-            cell[0] = index
-        elif char == end:
-            if inRange:  # 只有前符号的（多半有问题）
-                cell[1] = index
-                indexList.append(cell)
-                cell = [0, 0]
-                inRange = False
-            else:
-                cell[0], cell[1] = 0, index
-                indexList.append(cell)
-                cell = [0, 0]
-                inRange = False
-        else:
-            pass
-
-    if inRange:  # 超出去了
-        cell[1] = len(string) - 1
-        indexList.append(cell)
-
-    return indexList
 
 
 def check(text, event):
@@ -92,11 +130,11 @@ def check(text, event):
 
         # 括号
         for targetMark in SPECIAL_RANGE:
-            for area in __findArea(strRow, targetMark[0], targetMark[1]):
+            for area in __findArea(strRow, targetMark[0], targetMark[1], targetMark[2]):
                 start, end = area[0], area[1]
                 target = text.get(f'{rowIndex+1}.{start}', f'{rowIndex+1}.{end+1}')
                 text.delete(f'{rowIndex+1}.{start}', f'{rowIndex+1}.{end+1}')
                 text.insert(f'{rowIndex+1}.{start}', target, 'bracket')
 
-    text.mark_set('insert', f'{insertRow}.{insertColumn}')  # 鼠标放回去
+    text.mark_set('insert', f'{insertRow}.{insertColumn}')  # FIXME wrong position at row end
     logEvent.emit(f'Finish checking ,insert position {insertRow}.{insertColumn}...')
