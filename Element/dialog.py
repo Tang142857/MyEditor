7ec448df9cca4cf41d9328d6bc80821fbda56c1d
Tang142857 <dfsa00001@qq.com>
2021-02-07 23:21:09 +0800

    add dialog._ask

--- Element/dialog.py
+++ Element/dialog.py
@@ -7,18 +7,29 @@ Copyright(c) DFSA Software Develop Center
 
 import tkinter
 import tkinter.filedialog
+from typing import Callable
 
 COMMON = ('Microsoft YaHei', 12)
 
 
-def _ask(master, title: str, message: str, **args):
+class ExitException(Exception):
+    def __init__(self, answer):
+        self.answer = answer
+
+
+def _ask(master, title: str, message: str, callback: Callable, **args):
+    """Ask a question and get answer."""
+    def getAnwser():
+        callback(answer.get())
+        root.destroy()
+
     root = tkinter.Toplevel(master=master)
 
     message = tkinter.Label(root, text=message, font=COMMON)
     answer = tkinter.StringVar()
     entrance = tkinter.Entry(root, textvariable=answer, font=COMMON)
-    okButton = tkinter.Button(root, text='Sure', command=None)
-    cancelButton = tkinter.Button(root, text='Cancel', command=None)
+    okButton = tkinter.Button(root, text='Sure', command=getAnwser)
+    cancelButton = tkinter.Button(root, text='Cancel', command=root.destroy)
 
     message.pack()
     entrance.pack()
@@ -26,7 +37,19 @@ def _ask(master, title: str, message: str, **args):
     cancelButton.pack()
 
     root.mainloop()
+    return ret
+
+
+def do():
+    _ask(win, 'test', 'test', r)
+
+
+def r(ans):
+    print(ans)
 
 
 if __name__ == '__main__':
-    _ask(None, 'test', 'test')
+    win = tkinter.Tk()
+    b = tkinter.Button(win, text='test', command=do)
+    b.pack()
+    win.mainloop()
